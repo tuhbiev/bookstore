ec69e1a1ea62d69dc3e35e4bb082ffb51441dc3f
tuhbiev <radis.ramisovich@gmail.com>
2021-04-12 17:59:53 +0300

    update login logout sign up

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -103,14 +103,12 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    #Admin
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    
+    #User management
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    #Local apps
+    path('accounts/', include('users.urls')),
+    path('', include('pages.urls')),
+
 ]
